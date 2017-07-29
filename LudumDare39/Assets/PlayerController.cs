99a1fa7df78acb1b1fe2c06b3f51c6b023ebb171
DESKTOP-BMVUJHI\jimiv <a@a.a>
2017-07-29 16:11:30 +0200

    Player movement en beginnetje met plugs oppakken

--- LudumDare39/Assets/PlayerController.cs
+++ LudumDare39/Assets/PlayerController.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
+    Rigidbody2D rb2d;
+    public float speed;
+    float plugcount;
+
+
     // Use this for initialization
     void Start()
     {
-
+        BoxCollider2D[] boxes = GetComponents<BoxCollider2D>();
+        BoxCollider2D plugtrigger = boxes[0];
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -20,6 +27,42 @@ public class PlayerController : MonoBehaviour
     /// <summary> Handles input and controls the player. </summary>
     private void HandleInput()
     {
+        Moving();
+        PlugHandler();
 
+
+
+    }
+
+    private void Moving()
+    {
+        float x, y;
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+        rb2d.AddForce(new Vector2(x * speed, y * speed));
+
+        //Drag
+        rb2d.velocity = new Vector2((rb2d.velocity.x * 0.3f),(rb2d.velocity.y * 0.3f));
+    }
+
+    private void PlugHandler()
+    {
+        if (plugcount >= 1 || Input.GetButtonDown("Jump"))
+        {
+            //pick up plugs
+        }
+    
+    }
+
+    private void OnTriggerEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Plug"))
+            plugcount++;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Plug"))
+            plugcount--;
     }
 }
