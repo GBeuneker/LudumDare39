4ebc639909b23476dae4e8126f8084fdc74b1987
Geert - BTO_Laptop <g.beuneker@gmail.com>
2017-07-30 16:58:11 +0200

    FUCKING UNITY ANIMATOR BULLSHIT ASS SYSTEM

--- LudumDare39/Assets/TitlescreenScript.cs
+++ LudumDare39/Assets/TitlescreenScript.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class TitlescreenScript : MonoBehaviour
 {
+    [SerializeField]
+    Animator titleAnimator;
 
     // Use this for initialization
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
+    }
 
+    public void ToCharacterSelection()
+    {
+        titleAnimator.SetTrigger("Exit");
+        titleAnimator.SetTrigger("ToCharacterSelection");
+    }
+
+    public void EnableButtons()
+    {
+        GameObject.Find("Start").SetActive(true);
     }
 }
