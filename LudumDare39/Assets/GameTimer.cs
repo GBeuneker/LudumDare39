708899ffd588937ca5905e0c3aea6de3dc0edf0a
DESKTOP-ULLNRSA\Summer Glou <jimivanoosten@hotmail.com>
2017-08-27 16:00:52 +0200

    IK BEN TIJD!

---
+++ LudumDare39/Assets/GameTimer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    public Text textTimer;
+    float timer;
+
+    // Use this for initialization
+    void Start()
+    {
+        timer = 120; //playtime
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        textTimer.text = string.Format("{0}:{1}", (int)(timer / 60), ((int)timer % 60).ToString("00"));
+        if (timer <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        
+    }
+}
