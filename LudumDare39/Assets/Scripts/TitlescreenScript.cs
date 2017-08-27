bc2be931f6978258d538d5267274a6239294b4d7
Geert - BTO_Laptop <g.beuneker@gmail.com>
2017-08-27 15:20:48 +0200

    Fixed titlescreen bugs

--- LudumDare39/Assets/Scripts/TitlescreenScript.cs
+++ LudumDare39/Assets/Scripts/TitlescreenScript.cs
@@ -1,11 +1,14 @@
 ﻿using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class TitlescreenScript : MonoBehaviour
 {
     [SerializeField]
-    Animator titleAnimator, startAnimator, menuAnimator, characterSelectAnimator;
+    Animator startAnimator = null, menuAnimator = null, characterSelectAnimator = null;
 
     private bool startedMenu = false;
 
@@ -20,10 +23,17 @@ public class TitlescreenScript : MonoBehaviour
         if (Input.anyKeyDown && !startedMenu)
             PressStart();
 
-        if (Input.GetButtonDown("Cancel_1"))
+        if (Input.GetButtonDown("Cancel_1") && Characters.Count > 0 && !Characters.Exists(c => c.PlayerNumber == PlayerNumber.Player_1 && c.IsReady))
         {
             ExitCharacterSelection();
         }
+    }
+
+    private void ActivateMenu()
+    {
+        menuAnimator.SetBool("Visible", true);
+        GameObject button = menuAnimator.gameObject.GetComponentInChildren<Button>(true).gameObject;
+        EventSystem.current.SetSelectedGameObject(button);
     }
 
     public void EnterCharacterSelection()
@@ -36,8 +46,7 @@ public class TitlescreenScript : MonoBehaviour
     {
         characterSelectAnimator.SetBool("Visible", false);
         characterSelectAnimator.GetComponentsInChildren<CharacterSelect>().ToList().ForEach(c => c.UnReadyPlayer());
-
-        menuAnimator.SetBool("Visible", true);
+        ActivateMenu();
     }
 
     public void PressStart()
@@ -45,22 +54,26 @@ public class TitlescreenScript : MonoBehaviour
         startedMenu = true;
 
         startAnimator.SetTrigger("Disappear");
-        menuAnimator.SetBool("Visible", true);
+        ActivateMenu();
     }
 
     public void StartGame()
     {
-        CharacterSelect[] characters = GetComponentsInChildren<CharacterSelect>();
         // Clear the list of active players
         GameInformation.Instance.ActivePlayers.Clear();
 
         // Add all the readied players to the list of active players
-        foreach (CharacterSelect character in characters)
+        foreach (CharacterSelect character in Characters)
         {
             if (character.IsReady)
                 GameInformation.Instance.ActivePlayers.Add(character.PlayerNumber);
         }
 
         SceneManager.LoadScene("Main");
+    }
+
+    private List<CharacterSelect> Characters
+    {
+        get { return GetComponentsInChildren<CharacterSelect>().ToList(); }
     }
 }
